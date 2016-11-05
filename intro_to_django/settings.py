91d6ef83b6e06141e8f86a321b1fa8e5a797f951
Mohammadreza Kasaiee <1tapciran@gmail.com>
2016-11-05 14:00:30 +0330

    Update settings.py

--- intro_to_django/settings.py
+++ intro_to_django/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'blog.context_processors.search',
             ],
         },
     },
